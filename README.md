6896068c63e29ebb48bef475e43f7282eafcedf0
Chris Dixon <chris.dixon@withsecure.com>
2023-05-22 14:41:07 +0100

    Second Update-Added Json dummy file, moved token into file that's not commited

---
+++ README.md
@@ -0,0 +1,2 @@
+g++ discordBot.cpp -o mybot -ldpp -std=c++17
+- To run file.
--- discordBot.cpp
+++ discordBot.cpp
@@ -1,9 +1,18 @@
 #include <dpp/dpp.h>
-
-const std::string    BOT_TOKEN    = "MTEwOTUwMzI4Njk1ODEwNDYwNg.GHLaMV.X9gBvNrVKJmXai-LpBkdhi-hAo0aChjmrQ_WPE";
+#include <iostream>
+#include <fstream>
+#include <string>
 
 int main() {
-    dpp::cluster bot(BOT_TOKEN);
+    std::string item_name;
+    std::ifstream myfile("token.txt");
+    if(myfile.is_open()){
+        myfile >> item_name;
+    }
+
+    const std::string BOT_TOKEN = item_name;
+    uint64_t intents = dpp::i_default_intents | dpp::i_message_content;
+    dpp::cluster bot(BOT_TOKEN, intents);
 
     bot.on_log(dpp::utility::cout_logger());
 
@@ -13,9 +22,20 @@ int main() {
         }
     });
 
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-        if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
+	    bot.on_message_create([&bot](const dpp::message_create_t &event) {
+        if (event.msg.content == "!file") {
+            // create a message
+            dpp::message msg(event.msg.channel_id, "farts");
+ 
+            // attach the image to the message
+            msg.add_file("cheese.jpg", dpp::utility::read_file("/home/linuxdev/Documents/cheese.jpg"));
+ 
+            dpp::embed embed;
+            embed.set_image("attachment://cheese.jpg"); // reference to the attached file
+            msg.add_embed(embed);
+ 
+            // send the message
+            bot.message_create(msg);
         }
     });
 
