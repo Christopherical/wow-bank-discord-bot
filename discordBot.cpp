02207c9eed1915266ea7b9dfbde37cf7008e8c8a
Chris Dixon <chris.dixon@withsecure.com>
2023-05-24 15:54:23 +0100

    Third Commit. Added embeds that iterate through json and produce coloured text

--- discordBot.cpp
+++ discordBot.cpp
@@ -1,21 +1,31 @@
+#define FMT_HEADER_ONLY
 #include <dpp/dpp.h>
+#include <dpp/colors.h>
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "json.hpp"
+#include "include/json.hpp"
+#include "include/fmt/include/fmt/color.h"
+
+using json = nlohmann::json;
 
 int main() {
-    using json = nlohmann::json;
     std::string botToken;
-    std::ifstream myBotToken("token.txt");
-    std::ifstream myJson("bank.json");
-    json bank_json = json::parse(myJson);
+    std::ifstream myBotToken("ids/token.txt");
     if(myBotToken.is_open()){
         myBotToken >> botToken;
     }
 
-    // Just testing to see how nlohmann works - TODO
-    std::cout << bank_json["Bank"]["BankTabs"]["One"].dump(1) << std::endl;
+    std::string guildToken;
+    std::ifstream myGuildToken("ids/snowflake.txt");
+    if(myGuildToken.is_open()){
+        myGuildToken >> guildToken;
+    }
+
+    std::ifstream myJson("data/bank.json");
+    json bankJson = json::parse(myJson);
+    json tabOne = bankJson["BankTabs"]["One"];
+    json tabTwo = bankJson["BankTabs"]["Two"];
 
     const std::string BOT_TOKEN = botToken;
     uint64_t intents = dpp::i_default_intents | dpp::i_message_content;
@@ -23,32 +33,36 @@ int main() {
 
     bot.on_log(dpp::utility::cout_logger());
 
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-        if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
-        }
-    });
+    bot.on_message_create([&bot, tabOne, tabTwo](const dpp::message_create_t& event) {
+         if (event.msg.content == "!bank") {
+            // First Bank tab.
+            dpp::embed embed = dpp::embed().set_color(dpp::colors::cinnabar);     
+            embed.set_title("Bank Tab One");
 
-	    bot.on_message_create([&bot](const dpp::message_create_t &event) {
-        if (event.msg.content == "!file") {
-            // create a message
-            dpp::message msg(event.msg.channel_id, "uploading_image");
- 
-            // attach the image to the message
-            msg.add_file("cheese.jpg", dpp::utility::read_file("/home/linuxdev/Documents/cheese.jpg"));
- 
-            dpp::embed embed;
-            embed.set_image("attachment://cheese.jpg"); // reference to the attached file
-            msg.add_embed(embed);
- 
-            // send the message
+            for (auto it = tabOne.begin(); it != tabOne.end(); ++it){
+                std::string formattedValue = fmt::format("```ansi\n\u001b[0;31m{}\n```",static_cast<std::string>(it.value()));
+                embed.add_field(it.key(), formattedValue, true);
+            }
+
+            // Second Bank Tab.
+            dpp::embed embed2 = dpp::embed().set_color(dpp::colors::jazzberry_jam);
+            embed2.set_title("Bank Tab Two");
+
+            for (auto it = tabTwo.begin(); it != tabTwo.end(); ++it){
+                std::string formattedValue = fmt::format("```ansi\n\u001b[0;34m{}\n```",static_cast<std::string>(it.value()));
+                embed2.add_field(it.key(), formattedValue, true);
+            }
+
+            dpp::message msg(event.msg.channel_id, embed);
+            dpp::message msg2(event.msg.channel_id, embed2);
             bot.message_create(msg);
+            bot.message_create(msg2);
         }
     });
 
-    bot.on_ready([&bot](const dpp::ready_t& event) {
+    bot.on_ready([&bot, guildToken](const dpp::ready_t& event) {
         if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+            bot.guild_command_create(dpp::slashcommand("test", "Test!", bot.me.id), guildToken);
         }
     });
 
